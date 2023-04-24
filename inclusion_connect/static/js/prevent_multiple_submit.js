b42626013682bd7bf2c632d28f062848ee7dd698
François Freitag <mail@franek.fr>
2023-04-24 13:58:28 +0200

    static: Remove Jquery dependency from script

--- inclusion_connect/static/js/prevent_multiple_submit.js
+++ inclusion_connect/static/js/prevent_multiple_submit.js
@@ -1,17 +1,19 @@
-htmx.onLoad((target) => {
-
+addEventListener("DOMContentLoaded", () => {
   // Prevent multiple submit client side.
-  // Never trust the client: validation must also happen server side.
   // Opt-in by adding a `js-prevent-multiple-submit` CSS class to a <form>.
 
   // Pay attention: in multi-step forms, the browser will remember the disabled
   // state of the button when clicking "Previous" in the browser, eventually
-  // disabling moving forward afterwards !
+  // disabling moving forward afterwards!
 
-  $('form.js-prevent-multiple-submit', target).on('submit', function () {
-      $(':submit', this).on('click', function () {
-          return false
-      })
+  document.querySelectorAll('form.js-prevent-multiple-submit').forEach((form) => {
+    let submitted = false;
+    function disableSubmit(event) {
+      if (submitted) {
+        event.preventDefault();
+      }
+      submitted = true;
+    }
+    form.addEventListener("submit", disableSubmit);
   });
-
-})
+});
