d78ae7c7e3f272e26705c19bb2178d449488bf59
François Freitag <mail@franek.fr>
2023-04-24 18:08:54 +0200

    Use a custom template tag

---
+++ inclusion_connect/utils/templatetags/inclusionconnect_fields.py
@@ -0,0 +1,20 @@
+from bootstrap4.templatetags.bootstrap4 import bootstrap_field
+from django import template
+
+
+register = template.Library()
+
+
+@register.simple_tag
+def password_field(form_field):
+    return bootstrap_field(
+        form_field,
+        form_group_class="form-group",
+        addon_after="""
+        <button class="btn btn-sm btn-link btn-ico" type="button" data-password="toggle">
+            <i class="ri-eye-line"></i>
+            <span>Afficher</span>
+        </button>
+        """,
+        addon_after_class=None,
+    )
