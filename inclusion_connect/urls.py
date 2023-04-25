6c886689d05ce605d7313490d6dce075db2720dd
François Freitag <mail@franek.fr>
2023-04-25 18:08:06 +0200

    Fix typo in urls
    URLs were included twice.

--- inclusion_connect/urls.py
+++ inclusion_connect/urls.py
@@ -41,4 +41,3 @@ if settings.DEBUG and "debug_toolbar" in settings.INSTALLED_APPS:
     import debug_toolbar
 
     urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
-    urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
