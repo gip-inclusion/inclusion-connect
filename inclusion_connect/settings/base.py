4e76ebe9dffccd41afebdf69527fcd79383906b2
François Freitag <mail@franek.fr>
2023-05-05 15:41:25 +0200

    Run tests closer to production
    Tests should not run in debug mode.

--- inclusion_connect/settings/base.py
+++ inclusion_connect/settings/base.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
+DEBUG = False
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "connect.inclusion.beta.gouv.fr").split(",")
 
