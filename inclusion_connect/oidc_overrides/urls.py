b100cf3551d32baaece707c8c00dda9af84713a7
Antoine LAURENT <antoine.c.laurent.pro@gmail.com>
2023-05-15 09:25:28 +0200

    oidc_overrides: Add missing trailing slash in oidc urls

--- inclusion_connect/oidc_overrides/urls.py
+++ inclusion_connect/oidc_overrides/urls.py
@@ -6,8 +6,8 @@ from inclusion_connect.oidc_overrides import views
 app_name = "oidc_overrides"
 
 urlpatterns = [
-    re_path(r"^authorize", views.AuthorizationView.as_view(), name="authorize"),
-    re_path(r"^registrations", views.RegistrationView.as_view(), name="registrations"),
-    re_path(r"^activation", views.ActivationView.as_view(), name="activation"),
-    re_path(r"^logout", views.LogoutView.as_view(), name="logout"),
+    re_path(r"^authorize/", views.AuthorizationView.as_view(), name="authorize"),
+    re_path(r"^registrations/", views.RegistrationView.as_view(), name="registrations"),
+    re_path(r"^activation/", views.ActivationView.as_view(), name="activation"),
+    re_path(r"^logout/", views.LogoutView.as_view(), name="logout"),
 ]
