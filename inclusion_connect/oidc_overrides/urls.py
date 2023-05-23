6dcd2c5862af7c0c90c5104aef0fb13ca30c8dc9
Antoine LAURENT <antoine.c.laurent.pro@gmail.com>
2023-05-23 15:15:35 +0200

    oidc_overrides: Clean entrypoints urls

--- inclusion_connect/oidc_overrides/urls.py
+++ inclusion_connect/oidc_overrides/urls.py
@@ -7,7 +7,7 @@ app_name = "oidc_overrides"
 
 urlpatterns = [
     re_path(r"^authorize/", views.AuthorizationView.as_view(), name="authorize"),
-    re_path(r"^registrations/", views.RegistrationView.as_view(), name="registrations"),
-    re_path(r"^activation/", views.ActivationView.as_view(), name="activation"),
+    re_path(r"^register/", views.RegistrationView.as_view(), name="register"),
+    re_path(r"^activate/", views.ActivationView.as_view(), name="activate"),
     re_path(r"^logout/", views.LogoutView.as_view(), name="logout"),
 ]
