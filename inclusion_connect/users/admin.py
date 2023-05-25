69fa884c2050b012a5a21678a38a9a63e3dde0e2
François Freitag <mail@franek.fr>
2023-05-25 15:11:04 +0200

    Simplify admin lookup of `EmailAddress`es
    The `search_fields` can follow relationships, allowing for a pretty neat
    cleanup.

--- inclusion_connect/users/admin.py
+++ inclusion_connect/users/admin.py
@@ -4,9 +4,8 @@ from django import forms
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin, forms as auth_forms
 from django.core.exceptions import ValidationError
-from django.db.models import Exists, F, OuterRef, Q
+from django.db.models import F
 from django.forms.formsets import DELETION_FIELD_NAME
-from django.utils.text import smart_split, unescape_string_literal
 
 from .models import EmailAddress, User
 
@@ -59,18 +58,7 @@ class UserAdmin(auth_admin.UserAdmin):
     readonly_fields = ["username", "email"]
     inlines = [EmailAddressInline]
     change_password_form = AdminPasswordChange
-
-    def get_search_results(self, request, queryset, search_term):
-        queryset, may_have_duplicates = super().get_search_results(request, queryset, search_term)
-        term_queries = []
-        for bit in smart_split(search_term):
-            if bit.startswith(('"', "'")) and bit[0] == bit[-1]:
-                bit = unescape_string_literal(bit)
-            term_queries.append(Q(email__icontains=bit))
-        queryset |= self.model.objects.filter(
-            Exists(EmailAddress.objects.filter(*term_queries, user_id=OuterRef("pk")))
-        )
-        return queryset, may_have_duplicates
+    search_fields = auth_admin.UserAdmin.search_fields + ("email_addresses__email",)
 
     def save_related(self, request, form, formsets, change):
         super().save_related(request, form, formsets, change)
