d26321a960b5a1006dc5c5f14e90effea9ab68af
Antoine LAURENT <antoine.c.laurent.pro@gmail.com>
2023-05-30 15:34:21 +0200

    tests: Use NoInlineClient to check html response

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,6 +14,30 @@ def pytest_collection_modifyitems(config, items):
             item.add_marker(pytest.mark.django_db)
 
 
+class NoInlineClient(Client):
+    def request(self, **request):
+        response = super().request(**request)
+        content_type = response["Content-Type"].split(";")[0]
+        if content_type == "text/html" and response.content:
+            content = response.content.decode(response.charset)
+            assert " onclick=" not in content
+            assert " onbeforeinput=" not in content
+            assert " onbeforeinput=" not in content
+            assert " onchange=" not in content
+            assert " oncopy=" not in content
+            assert " oncut=" not in content
+            assert " ondrag=" not in content
+            assert " ondragend=" not in content
+            assert " ondragenter=" not in content
+            assert " ondragleave=" not in content
+            assert " ondragover=" not in content
+            assert " ondragstart=" not in content
+            assert " ondrop=" not in content
+            assert " oninput=" not in content
+            assert "<script>" not in content
+        return response
+
+
 @pytest.fixture
 def client(django_capture_on_commit_callbacks):
     class ExecuteOnCommitCallbacksClient(Client):
@@ -21,4 +45,7 @@ def client(django_capture_on_commit_callbacks):
             with django_capture_on_commit_callbacks(execute=True):
                 return super().request(**request)
 
-    return ExecuteOnCommitCallbacksClient()
+    class InclusionConnectTestClient(ExecuteOnCommitCallbacksClient, NoInlineClient):
+        pass
+
+    return InclusionConnectTestClient()
