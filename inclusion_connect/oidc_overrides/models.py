6c7a903f9444265341f79ee173d007ac442f860b
Antoine LAURENT <antoine.c.laurent.pro@gmail.com>
2023-06-05 10:01:53 +0200

    oidc_overrides: Use new RP logout view

--- inclusion_connect/oidc_overrides/models.py
+++ inclusion_connect/oidc_overrides/models.py
@@ -1,18 +1,24 @@
+from urllib.parse import urlparse
+
+from django.conf import settings
 from oauth2_provider.models import AbstractApplication
+
+
+def check_uri(allowed_uris, uri):
+    for allowed_uri in allowed_uris:
+        if allowed_uri == "*":
+            continue
+        allowed_path = urlparse(allowed_uri).path
+        if allowed_path and allowed_path[-1] == "*" and uri.startswith(allowed_uri[:-1]):
+            return True
+    return False
 
 
 class Application(AbstractApplication):
     skip_authorization = True
 
     def redirect_uri_allowed(self, uri):
-        """
-        Checks if given url is one of the items in :attr:`redirect_uris` string
-
-        :param uri: Url to check
-        """
-
-        # Enable full wildcard
-        if "*" in self.redirect_uris.split():
+        if settings.ALLOW_ALL_REDIRECT_URIS:
             return True
 
         # Else check default implementation
@@ -20,7 +26,15 @@ class Application(AbstractApplication):
             return True
 
         # Check if path ends with wildcard
-        for allowed_uri in self.redirect_uris.split():
-            if allowed_uri[-1] == "*" and uri.startswith(allowed_uri[:-1]):
-                return True
-        return False
+        return check_uri(self.redirect_uris.split(), uri)
+
+    def post_logout_redirect_uri_allowed(self, uri):
+        if settings.ALLOW_ALL_REDIRECT_URIS:
+            return True
+
+        # Else check default implementation
+        if super().post_logout_redirect_uri_allowed(uri):
+            return True
+
+        # Check if path ends with wildcard
+        return check_uri(self.post_logout_redirect_uris.split(), uri)
