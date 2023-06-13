04e47a5a88fbab96cb54cfae0a199d51856248a0
François Freitag <mail@franek.fr>
2023-06-13 14:24:49 +0200

    Preserve redirect_uri when showing CSRF failure
    Allows users to go back to their application.

---
+++ inclusion_connect/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponseForbidden
+from django.template import loader
+from django.urls import reverse
+
+from inclusion_connect.utils.urls import add_url_params
+
+
+def csrf_failure(request, template_name="403_csrf.html", **kwargs):
+    template = loader.get_template(template_name)
+    context = {
+        "edit_user_info_url": add_url_params(
+            reverse("accounts:edit_user_info"), {"redirect_uri": request.GET.get("referrer_uri")}
+        )
+    }
+    return HttpResponseForbidden(template.render(context))
