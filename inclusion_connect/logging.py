7222d772f8aad17dbf85f01b0b9e944c1d514b96
François Freitag <mail@franek.fr>
2023-06-15 10:39:34 +0200

    Send logs to ElasticSearch
    Logs are buffered by the application, and sent either:
    - when the buffer is full, or
    - after 5 seconds without logs
    
    `flush()` does not send synchronously to ElasticSearch, to avoid
    blocking on `emit()` (which calls `flush()` synchronously when capacity
    is reached).
    
    The `send_after_inactive_for_secs` is purposely not too large, to
    increase the chances of flushing before the new logs come in. In the
    worst case, with a capacity of 500, the logs can be retained 500 * 2s =
    1000 seconds (a bit more than 15 minutes).
    
    The Elasticsearch client implements a power of 2 backoff. With 10 max
    retries, the app will wait for at 2^11-1 = 2047 seconds for
    ElasticSearch to be back.

--- inclusion_connect/logging.py
+++ inclusion_connect/logging.py
@@ -1,3 +1,8 @@
+import logging.handlers
+import threading
+
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
 from pythonjsonlogger import jsonlogger
 
 from inclusion_connect.utils.oidc import oidc_params
@@ -22,3 +27,48 @@ class JsonFormatter(jsonlogger.JsonFormatter):
         super().add_fields(log_record, record, message_dict)
         for record_attr in ["name", "levelname"]:
             log_record[record_attr] = getattr(record, record_attr)
+
+
+class ElasticSearchHandler(logging.handlers.BufferingHandler):
+    timer = None
+
+    def __init__(  # noqa: PLR0913 Too many arguments to function call.
+        self,
+        *,
+        capacity,
+        send_after_inactive_for_secs=2.0,
+        host,
+        index_name,
+        es_timeout_secs=5,
+    ):
+        """
+        Inspired from https://github.com/cmanaha/python-elasticsearch-logger/blob/master/cmreslogging/handlers.py.
+
+        :param int capacity: Max number of log records before sending to ElasticSearch.
+        :param float send_after_inactive_for_secs: Send to ElasticSearch if no records
+            are emitted in send_after_inactive_for_secs seconds.
+        """
+        super().__init__(capacity)
+        self.send_after_inactive_for_secs = send_after_inactive_for_secs
+        self.es_client = Elasticsearch(host, http_compress=True, request_timeout=es_timeout_secs, max_retries=10)
+        self.index_name = index_name
+
+    def emit(self, record):
+        if self.timer and self.timer.is_alive():
+            self.timer.cancel()
+        formatted_record = self.format(record)
+        super().emit(formatted_record)
+        self.timer = threading.Timer(self.send_after_inactive_for_secs, self.flush)
+        self.timer.start()
+
+    def send_to_elastic(self, log_buffer):
+        actions = ({"_source": log} for log in log_buffer)
+        bulk(client=self.es_client, actions=actions, index=self.index_name, stats_only=True)
+
+    def flush(self):
+        with self.lock:
+            log_buffer = self.buffer
+            self.buffer = []
+        if log_buffer:
+            t = threading.Thread(target=self.send_to_elastic, args=(log_buffer,), daemon=False)
+            t.start()
