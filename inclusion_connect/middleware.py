3dfa33c782fcc35876f0a70a4d82211188a2e0fb
Antoine LAURENT <antoine.c.laurent.pro@gmail.com>
2023-06-19 14:45:58 +0200

    inclusion_connect: Forbid staff users from using the SSO

--- inclusion_connect/middleware.py
+++ inclusion_connect/middleware.py
@@ -1,4 +1,7 @@
+from django.core.exceptions import PermissionDenied
+from django.urls import reverse
 from django.utils.cache import add_never_cache_headers
+from django.utils.html import format_html
 
 
 def never_cache(get_response):
@@ -7,5 +10,22 @@ def never_cache(get_response):
         if request.user.is_authenticated:
             add_never_cache_headers(response)
         return response
+
+    return middleware
+
+
+def limit_staff_users_to_admin(get_response):
+    def middleware(request):
+        user = request.user
+
+        if user.is_staff and not request.path.startswith("/admin/"):
+            exception = format_html(
+                "Les comptes administrateurs n'ont pas accès à cette page.<br>"
+                '<a href="{}">Vous pouvez-vous déconnecter ici.</a>',
+                reverse("admin:logout"),
+            )
+            raise PermissionDenied(exception)
+
+        return get_response(request)
 
     return middleware
