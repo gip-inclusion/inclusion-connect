10f6d817f52a68cc3af1f8d177485c90d7413220
Antoine LAURENT <antoine.c.laurent.pro@gmail.com>
2023-07-24 17:32:54 +0200

    script: Add reset script to easily clean prod DB after migration rehearsal

---
+++ scripts/clean_kc_import.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import os
+import pathlib
+import sys
+
+
+sys.path.append(str(pathlib.Path(__file__).parent.parent))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inclusion_connect.settings.dev")
+import django
+
+
+django.setup()
+
+from django.db import connection
+
+
+with connection.cursor() as cursor:
+    cursor.execute(
+        """
+        TRUNCATE
+            keycloak_compat_jwthashsecret,
+            oidc_overrides_application,
+            stats_stats,
+            users_emailaddress,
+            users_user,
+            users_userapplicationlink
+        CASCADE
+        """
+    )
+    for sequence in [
+        "users_user_groups_id_seq",
+        "users_user_user_permissions_id_seq",
+        "django_admin_log_id_seq",
+        "oauth2_provider_accesstoken_id_seq",
+        "oauth2_provider_grant_id_seq",
+        "oauth2_provider_refreshtoken_id_seq",
+        "oauth2_provider_idtoken_id_seq",
+    ]:
+        cursor.execute(f"ALTER SEQUENCE {sequence} RESTART WITH 1")
