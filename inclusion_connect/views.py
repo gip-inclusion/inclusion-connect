268afbf3790f6c4cf0be8d7c938f5b572961764a
Antoine LAURENT <antoine.c.laurent.pro@gmail.com>
2023-07-27 16:05:04 +0200

    Fix csrf view
    We need to use the context returned by the context_processors

--- inclusion_connect/views.py
+++ inclusion_connect/views.py
@@ -1,19 +1,16 @@
-from django.http import HttpResponseForbidden
 from django.shortcuts import render
-from django.template import loader
 from django.urls import reverse
 
 from inclusion_connect.utils.urls import add_url_params
 
 
 def csrf_failure(request, template_name="403_csrf.html", **kwargs):
-    template = loader.get_template(template_name)
     context = {
         "edit_user_info_url": add_url_params(
             reverse("accounts:edit_user_info"), {"redirect_uri": request.GET.get("referrer_uri")}
         )
     }
-    return HttpResponseForbidden(template.render(context))
+    return render(request, template_name, context=context, status=403)
 
 
 def home(request, template_name="homepage.html", **kwargs):
