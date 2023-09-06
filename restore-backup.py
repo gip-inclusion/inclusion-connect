508998902d830672d546602dbdbecb1d7abb7f3d
François Freitag <mail@franek.fr>
2023-09-06 12:35:27 +0200

    Only restore database when prod is not on Scalingo
    When the disaster recovery plan is active, production traffic is served
    by Scalingo. In that case, we certainly do NOT want to restore the
    latest backup on Scalingo during a deploy.

--- restore-backup.py
+++ restore-backup.py
@@ -13,6 +13,8 @@ from pathlib import Path
 from string import Template
 from tempfile import TemporaryDirectory
 
+import dns.resolver
+
 
 RCLONE_CONFIG = """\
 [scaleway]
@@ -76,6 +78,15 @@ def write_list_without_spatial_sys_ref(backup_file, pg_restore_list):
 
 
 def main(backup_name):
+    [cloud_provider_domain] = dns.resolver.resolve("connect.inclusion.beta.gouv.fr", "CNAME").rrset
+    cloud_provider_domain = str(cloud_provider_domain)
+    clevercloud_domain = "domain.par.clever-cloud.com."
+    scalingo_domain = "c5-inclusion-connect.osc-secnum-fr1.scalingo.io."
+    assert cloud_provider_domain in [clevercloud_domain, scalingo_domain]
+    if cloud_provider_domain == scalingo_domain:
+        print("Scalingo is currently serving traffic. Don’t touch its database.")
+        return
+
     print("Installing rclone…", file=sys.stderr)
     install_rclone()
 
