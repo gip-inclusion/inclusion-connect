bdb01d093198584df1c272704366e35900c5830e
François Freitag <mail@franek.fr>
2023-10-10 10:57:43 +0200

    Drop preserve_url argument from accounts.helpers.login
    Unused.

--- inclusion_connect/accounts/helpers.py
+++ inclusion_connect/accounts/helpers.py
@@ -1,11 +1,11 @@
 from django.contrib import auth
 
 
-def login(request, user, preserve_url=True):
+def login(request, user):
     """
-    Log the user and preserve the next url if required (as login again flushes the session)
+    Log the user and preserve the next url (as login again flushes the session)
     """
     next_url = request.session.get("next_url")
     auth.login(request, user)
-    if next_url and preserve_url:
+    if next_url:
         request.session["next_url"] = next_url
