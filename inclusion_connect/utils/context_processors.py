a95b6bbd1d737b934a0e6f6a6bd4d156fca0f078
Antoine LAURENT <antoine.c.laurent.pro@gmail.com>
2023-10-20 12:25:28 +0200

    accounts: Warn users about PEAMA staging environment

--- inclusion_connect/utils/context_processors.py
+++ inclusion_connect/utils/context_processors.py
@@ -3,5 +3,12 @@ from django.conf import settings
 
 def expose_settings(*args):
     return {
-        key: getattr(settings, key) for key in ["FAQ_URL", "PRIVACY_POLICY_PATH", "TERMS_PATH", "LEGAL_NOTICES_PATH"]
+        key: getattr(settings, key)
+        for key in [
+            "FAQ_URL",
+            "PRIVACY_POLICY_PATH",
+            "TERMS_PATH",
+            "LEGAL_NOTICES_PATH",
+            "PEAMA_STAGING",
+        ]
     }
