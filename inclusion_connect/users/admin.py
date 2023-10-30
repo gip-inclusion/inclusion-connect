1cee783377a5569810e3b3ff241d58bb4d78a3f2
Antoine LAURENT <antoine.c.laurent.pro@gmail.com>
2023-10-30 06:11:46 +0100

    users: Block support users from editing superusers

--- inclusion_connect/users/admin.py
+++ inclusion_connect/users/admin.py
@@ -255,13 +255,14 @@ class UserAdmin(auth_admin.UserAdmin):
         fieldsets = super().get_fieldsets(request, obj)
         is_change_form = obj is not None
         if is_change_form:
-            assert fieldsets[0] == (None, {"fields": ("username", "password")})
-            assert fieldsets[1] == ("Informations personnelles", {"fields": ("first_name", "last_name", "email")})
             fieldsets = list(copy.deepcopy(fieldsets))
-            if not obj.federation:
-                fieldsets[0][1]["fields"] = ("username", "must_reset_password")
-            else:
+            assert fieldsets[0] == (None, {"fields": ("username", "password")})
+            if obj.federation or not self.has_change_permission(request, obj):
                 fieldsets[0][1]["fields"] = ("username",)
+            else:
+                fieldsets[0][1]["fields"] = ("username", "must_reset_password")
+
+            assert fieldsets[1] == ("Informations personnelles", {"fields": ("first_name", "last_name", "email")})
             fieldsets[1][1]["fields"] += ("confirm_email",)
 
             fieldsets.append(("CGU", {"fields": ["terms_accepted_at"]}))
@@ -300,3 +301,8 @@ class UserAdmin(auth_admin.UserAdmin):
                 )
             )
         )
+
+    def has_change_permission(self, request, obj=None):
+        if getattr(obj, "is_superuser", False) and not request.user.is_superuser:
+            return False
+        return super().has_change_permission(request, obj)
