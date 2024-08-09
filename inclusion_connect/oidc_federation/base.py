b7aa84012535bfb6ebea21647d95744b302fc246
Antoine LAURENT <antoine.c.laurent.pro@gmail.com>
2024-08-09 07:20:11 +0200

    ProConnect migration: No more federated account creation

--- inclusion_connect/oidc_federation/base.py
+++ inclusion_connect/oidc_federation/base.py
@@ -1,18 +1,17 @@
 import logging
 from functools import partial
 
+from django.conf import settings
 from django.contrib import messages
-from django.core.exceptions import SuspiciousOperation
 from django.db import transaction
 from django.db.models import Q
-from django.forms.models import model_to_dict
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.utils.html import format_html
 from mozilla_django_oidc import auth, views
 
 from inclusion_connect.accounts.views import LoginView
 from inclusion_connect.logging import log_data
-from inclusion_connect.users.models import EmailAddress
 from inclusion_connect.utils.oidc import get_next_url
 
 
@@ -35,7 +34,9 @@ class OIDCAuthenticationCallbackView(ConfigMixin, views.OIDCAuthenticationCallba
         return get_next_url(self.request)
 
     def login_failure(self):
-        messages.error(self.request, "La connexion n'a pas fonctionné.")
+        # Don't add message if there's already the "no register" message
+        if not self.request._messages._queued_messages:
+            messages.error(self.request, "La connexion n'a pas fonctionné.")
         return HttpResponseRedirect(reverse("accounts:login"))
 
 
@@ -53,6 +54,10 @@ class OIDCAuthenticationBackend(ConfigMixin, auth.OIDCAuthenticationBackend):
     required_claims = ["email", "given_name", "family_name", "sub"]
     additionnal_claims = []
 
+    def authenticate(self, request, **kwargs):
+        self.request = request
+        return super().authenticate(request, **kwargs)
+
     def get_additional_data(self, claims):
         return {k: v for k, v in sorted(claims.items()) if k in self.additionnal_claims}
 
@@ -60,101 +65,48 @@ class OIDCAuthenticationBackend(ConfigMixin, auth.OIDCAuthenticationBackend):
         return Q(email__iexact=email)
 
     def filter_users_by_claims(self, claims):
-        sub_users = self.UserModel.objects.filter(federation_sub=claims["sub"], federation=self.name)
-        if sub_users:
-            return sub_users
-
-        email_q = self.email_lookup_q(claims["email"])
-        try:
-            user = EmailAddress.objects.select_related("user").get(email_q).user
-        except EmailAddress.DoesNotExist:
-            return []
-        if user.federation is not None:
-            log = log_data(self.request)
-            log["email"] = user.email
-            log["user"] = user.pk
-            log["event"] = f"{LoginView.EVENT_NAME}_error"
-            log["federation"] = self.name
-            transaction.on_commit(partial(logger.info, log))
-            raise SuspiciousOperation(
-                f"email={claims['email']} from federation={self.name} is already used by {user.federation}"
-            )
-        return [user]
+        return self.UserModel.objects.filter(federation_sub=claims["sub"], federation=self.name)
 
     def get_userinfo(self, access_token, id_token, payload):
         return super().get_userinfo(access_token, id_token, payload) | {"id_token": id_token}
 
     def create_user(self, claims):
-        user = self.UserModel.objects.create(
-            email=claims["email"],
-            first_name=claims["given_name"],
-            last_name=claims["family_name"],
-            federation_sub=claims["sub"],
-            federation=self.name,
-            federation_data=self.get_additional_data(claims),
-            federation_id_token_hint=claims["id_token"],
-        )
-        email_address = EmailAddress(user=user, email=user.email)
-        email_address.verify()
         log = log_data(self.request)
-        log["email"] = user.email
-        log["user"] = user.pk
-        log["event"] = "register"
+        log["email"] = claims["email"]
+        log["event"] = "register_error"
         log["federation"] = self.name
         transaction.on_commit(partial(logger.info, log))
 
-        return user
+        messages.error(
+            self.request,
+            format_html(
+                "La création de votre compte a échoué. Depuis le 1er octobre 2024, date du passage à ProConnect, "
+                "vous ne pouvez plus créer de compte Inclusion Connect. "
+                "Veuillez vous rapprocher de votre fournisseur de service pour plus d’informations sur la mise à "
+                "disposition de ProConnect, qui doit intervenir dans les meilleurs délais. "
+                '<a href="{}" target="_blank" rel="noopener">'
+                "Besoin d’aide</a> ?",
+                settings.MIGRATION_PAGE_URL,
+            ),
+        )
+
+        # No user creation
 
     def update_user(self, user, claims):
-        if (
-            user.federation
-            and user.email != claims["email"]
-            and EmailAddress.objects.filter(email=claims["email"]).exists()
-        ):
-            # Updating the email will crash because it"s already used. Don't update for now
-            user.new_email_already_used = claims["email"]
-            user.save()
-        else:
-            old_user_data = model_to_dict(user)
-            user.email = claims["email"]
-            user.first_name = claims["given_name"]
-            user.last_name = claims["family_name"]
-            user.federation_sub = claims["sub"]
-            user.federation = self.name
-            user.federation_data = self.get_additional_data(claims)
-            user.federation_id_token_hint = claims["id_token"]
-            user.save()
-            new_user_data = model_to_dict(user)
-
-            if old_user_data["email"] == "":
-                user.email_addresses.filter(email=claims["email"]).get().verify()
-            elif old_user_data["email"] != new_user_data["email"]:
-                email_address = EmailAddress(user=user, email=user.email)
-                email_address.verify()
-
         log = log_data(self.request)
         log["email"] = user.email
         log["user"] = user.pk
         log["event"] = LoginView.EVENT_NAME
         log["federation"] = self.name
+
         transaction.on_commit(partial(logger.info, log))
 
-        if user.new_email_already_used:
-            log = log_data(self.request)
-            log["event"] = "edit_user_info_error"
-            log["user"] = user.pk
-            log["errors"] = {"already_used_email": user.new_email_already_used}
-            transaction.on_commit(partial(logger.info, log))
-        else:
-            log = log_data(self.request)
-            log["event"] = "edit_user_info"
-            log["user"] = user.pk
-            for key in old_user_data.keys():
-                if old_user_data[key] != new_user_data[key]:
-                    log[f"old_{key}"] = old_user_data[key]
-                    log[f"new_{key}"] = new_user_data[key]
-            transaction.on_commit(partial(logger.info, log))
+        # Only store id_token_hint and update federation_data
+        user.federation_id_token_hint = claims["id_token"]
+        user.federation_data = self.get_additional_data(claims)
+        user.save()
 
+        # No more user update either
         return user
 
     def verify_claims(self, claims):
