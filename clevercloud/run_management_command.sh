3ee2b246082cea3e688eda8b137e231f5afe6af6
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2024-10-12 06:44:30 +0200

    clevercloud: Fix management command script
    We don't need CRON_ENABLED since we want it to be enabled everywhere

--- clevercloud/run_management_command.sh
+++ clevercloud/run_management_command.sh
@@ -2,8 +2,10 @@
 
 set -ue
 
-if [[ "$CRON_ENABLED" != "1" ]]; then
-    echo "Crons not enabled."
+# About clever cloud cronjobs:
+# https://www.clever-cloud.com/doc/administrate/cron/#deduplicating-crons
+if [[ "$INSTANCE_NUMBER" != "0" ]]; then
+    echo "Instance number is ${INSTANCE_NUMBER}. Stop here."
     exit 0
 fi
 
