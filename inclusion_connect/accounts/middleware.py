49b52b99fcb5e1670023c3e26453fdf289d1e7a1
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2026-05-19 20:27:58 +0200

    accounts: Rename required_action_url into next_action_url

--- inclusion_connect/accounts/middleware.py
+++ inclusion_connect/accounts/middleware.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-from inclusion_connect.accounts.helpers import required_action_url
+from inclusion_connect.accounts.helpers import next_action_url
 
 
 def post_login_actions(get_response):
@@ -12,9 +12,9 @@ def post_login_actions(get_response):
         path_is_whitelisted = request.path in whitelisted_urls
 
         if user.is_authenticated and path_is_whitelisted is False:
-            next_action_url = required_action_url(request)
-            if next_action_url and not request.path == next_action_url:
-                return HttpResponseRedirect(next_action_url)
+            next_url = next_action_url(request)
+            if next_url and not request.path == next_url:
+                return HttpResponseRedirect(next_url)
 
         return get_response(request)
 
