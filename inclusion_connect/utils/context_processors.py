5aefbb4666711b88dbe966f6dd6a39e54c86e86a
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2026-07-02 16:15:25 +0200

    feat: Add banner in demo mode

---
+++ inclusion_connect/utils/context_processors.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+
+
+def expose_settings(request):
+    """
+    Put things into the context to make them available in templates.
+    https://docs.djangoproject.com/en/4.1/ref/templates/api/#using-requestcontext
+    """
+
+    return {"DEMO_MODE": settings.DEMO_MODE}
