2cad8458ed6fb7158532957428324b8bbdaa2d18
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2026-09-08 19:27:19 +0200

    tests: Stop setting a default client_secret
    Let the factory generate a random secret : django-oauth-toolkit 3.4.1
    requires a correct unhashed client secret to make the HS256 signing.

--- tests/oidc_overrides/factories.py
+++ tests/oidc_overrides/factories.py
@@ -1,17 +1,6 @@
-import functools
-
 import factory
-from django.contrib.auth.hashers import make_password
 
 from inclusion_connect.oidc_overrides.models import Application
-
-
-DEFAULT_CLIENT_SECRET = "S3cr3t_KeY"
-
-
-@functools.cache
-def default_client_secret():
-    return make_password(DEFAULT_CLIENT_SECRET)
 
 
 class ApplicationFactory(factory.django.DjangoModelFactory):
@@ -24,5 +13,5 @@ class ApplicationFactory(factory.django.DjangoModelFactory):
     authorization_grant_type = Application.GRANT_AUTHORIZATION_CODE
     name = factory.Faker("company", locale="fr_FR")
     algorithm = Application.HS256_ALGORITHM
-    client_secret = default_client_secret()
     client_id = factory.Sequence("client_#{}".format)
+    hash_client_secret = False
